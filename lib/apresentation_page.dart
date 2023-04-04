9ff520d54f2cca7c0b2e15f387bd69e1469a46d3
Julio Cesar de Jesus <cesar.juliojc007@gmail.com>
2023-04-04 18:53:08 -0300

    fature [task: CES-2301]: add new page and scaffold

---
+++ lib/apresentation_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ApresentationPage extends StatelessWidget {
+  const ApresentationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple,
+      body: Container(
+        height: 50,
+        width: 50,
+        color: Colors.blue,
+        child: Column(),
+      ),
+    );
+  }
+}
