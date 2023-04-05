2ff3cea5599e090b06d88751448edc06f0d0cf35
Julio Cesar de Jesus <cesar.juliojc007@gmail.com>
2023-04-05 12:22:32 -0300

    feature[task: CES-2302]: Add images and layout widget

--- lib/apresentation_page.dart
+++ lib/apresentation_page.dart
@@ -6,12 +6,57 @@ class ApresentationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepPurple,
-      body: Container(
-        height: 50,
-        width: 50,
-        color: Colors.blue,
-        child: Column(),
+      body: SafeArea(
+        child: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/image/Templo.jpg'),
+              fit: BoxFit.cover,
+              opacity: 0.20,
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(50),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 65,
+                      height: 100,
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('assets/image/logo_comuna.png'),
+                            fit: BoxFit.fitHeight),
+                      ),
+                      child: const Text(''),
+                    ),
+                    Container(
+                      width: 250,
+                      height: 100,
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(
+                                'assets/image/comunidade_evangelica.png'),
+                            fit: BoxFit.fitHeight),
+                      ),
+                      child: const Text(''),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.blue,
+                  child: const Text('data'),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
