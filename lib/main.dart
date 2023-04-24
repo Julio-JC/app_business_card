d66866f5765abfce9f6ad11a94e140bde8662848
Julio Cesar de Jesus <cesar.juliojc007@gmail.com>
2023-04-24 13:59:52 -0300

    feature [task: CES-2302]: Add configuration for sending the App to the store and by email

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:app_business_card/page/apresentation_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_config/flutter_config.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await FlutterConfig.loadEnvVariables();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
   runApp(const MyApp());
 }
